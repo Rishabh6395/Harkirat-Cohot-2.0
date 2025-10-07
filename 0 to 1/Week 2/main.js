401bf6451c6b17e9326284e3d2ea422e7a121434
Rishabh <rishabhbhatt956@gmail.com>
2025-10-08 04:22:04 +0530

    playing with some basic comands in node and terminal via docker

--- 0 to 1/Week 2/main.js
+++ 0 to 1/Week 2/main.js
@@ -1,10 +1,12 @@
 // Creating HTTP Server
 
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(bodyParser.json())
 
 app.get('/', function(req, res){
     res.send("<b> Hello rishabh</b>")
