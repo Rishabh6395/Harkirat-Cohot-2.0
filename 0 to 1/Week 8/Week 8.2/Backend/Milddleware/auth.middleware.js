c17808b61fd52d68330b6476f011d4cdac962dec
Rishabh <rishabhbhatt956@gmail.com>
2025-11-13 04:53:09 +0530

    added some authentication with jwt and added authmiddleware. pending

---
+++ 0 to 1/Week 8/Week 8.2/Backend/Milddleware/auth.middleware.js
@@ -0,0 +1,27 @@
+const {JWT_SCRETE} = require("../config.js")
+const jwt = require("jsonwebtoken")
+
+const authMiddleware = (req, res, next) => {
+    const authHeaders = req.headers.authorization;
+
+    if(!authorization || !authHeaders.startsWith('Bearer')){
+        return res.status(403).json({
+            msg: "something is up!!"
+        })
+    }
+
+    const token = authHeaders.split('')[1];
+
+    try{
+        const decoded = jwt.verify(token, JWT_SCRETE)
+        req.userId = decoded.userID;
+
+        next()
+    }
+    catch(err){
+        return res.status(403).json({})
+    }
+}
+module.exports = {
+    authMiddleware
+}
