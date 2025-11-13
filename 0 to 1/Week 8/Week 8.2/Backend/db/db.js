44664dfc558913ba458e7d63f7fea7f3700e2e0e
Rishabh <rishabhbhatt956@gmail.com>
2025-11-14 04:26:23 +0530

    pening. will code tommorow

--- 0 to 1/Week 8/Week 8.2/Backend/db/db.js
+++ 0 to 1/Week 8/Week 8.2/Backend/db/db.js
@@ -21,8 +21,22 @@ const UserSchema = mongoose.Schema({
     }
 })
 
+const accountSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+})
+
+const Account = mongoose.model("Account", accountSchema)
 const User = mongoose.model("User", UserSchema)
 
 module.exports = {
-    User
+    User,
+    Account
 }
