88d6fbd18fcdc9d865ee49921919c19717fad92c
Rishabh <rishabhbhatt956@gmail.com>
2025-11-28 05:03:36 +0530

    initializing prima ORM with neonDB and created User and Todo table

---
+++ 0 to 1/Week 10/week 10.2/prisma/migrations/20251127231259_user_and_todo_added/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "done" BOOLEAN NOT NULL DEFAULT false,
+    "description" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Todo_title_key" ON "Todo"("title");
