90bbce6b877732a6f6193be6b451485e37624d52
Rishabh <rishabhbhatt956@gmail.com>
2025-11-29 11:02:17 +0530

    update user with prisma

--- 0 to 1/Week 10/week 10.2/src/index.ts
+++ 0 to 1/Week 10/week 10.2/src/index.ts
@@ -2,17 +2,41 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({} as any);
 
-async function insertUser(email: string, password: string, firstName: string, lastName: string) {
-    const res = await prisma.user.upsert({
-        where: { email },
-        update: {},
-        create: {
-            email,
-            password,
+// async function insertUser(email: string, password: string, firstName: string, lastName: string) {
+//     const res = await prisma.user.upsert({
+//         where: { email },
+//         update: {},
+//         create: {
+//             email,
+//             password,
+//             firstName,
+//             lastName
+//         }
+//     })
+//     console.log(res)
+// }
+// insertUser('rishabh1@gmail.com', 'rishabh123', 'Rishabh', 'Bhatt')
+
+
+interface UpdateParams {
+    firstName: string
+    lastName: string
+}
+
+async function updateUser(email:string, {
+    firstName,
+    lastName
+}: UpdateParams){
+    const res = await prisma.user.update({
+        where: {email: email},
+        data: {
             firstName,
             lastName
         }
     })
     console.log(res)
-}
-insertUser('rishabh@gmail.com', 'rishabh123', 'Rishabh', 'Bhatt')
+} 
+updateUser('rishabh1@gmail.com',{
+    firstName: "rishu",
+    lastName: "lol"
+})
