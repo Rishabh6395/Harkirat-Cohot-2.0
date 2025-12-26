54e9c3e19cb6889e16c88cf1ce234789f02cbe05
Rishabh <rishabhbhatt956@gmail.com>
2025-12-26 10:28:49 +0530

    Intro of Backend with some routes

---
+++ 0 to 1/Week 14/Week 14.2/newapp/app/api/user/route.ts
@@ -0,0 +1,15 @@
+
+export function GET(){
+    // db logic
+    return Response.json({
+        email: "rishabh.bhatt@gmail.com",
+        name: "rishabh"
+    })
+}
+export function POST(){
+    // db logic
+    return Response.json({
+        email: "rishabh.bhatt@gmail.com",
+        name: "rishabh"
+    })
+}
---
+++ 0 to 1/Week 14/Week 14.2/newapp/app/page.tsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+import Image from "next/image";
+
+async function getUserDetails()  {
+  await new Promise ((r) => setTimeout(r,5000))
+  const reponse = await axios.get("http://localhost:3000/api/user")
+  return reponse.data;
+}
+
+export default async function Home() {
+  const userDetails = await getUserDetails()
+  return (
+    <div className="">
+      hi
+      
+      {userDetails.name}
+      {userDetails.email}
+    </div>
+  );
+}
