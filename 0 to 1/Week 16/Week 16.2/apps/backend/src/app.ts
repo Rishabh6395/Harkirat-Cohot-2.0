eef25a229cf0ddb7563a60cbb3d1f20ffa84b02b
Rishabh <rishabhbhatt956@gmail.com>
2026-01-25 10:19:24 +0530

    almost done backend with turborepo 01:16:00

--- 0 to 1/Week 16/Week 16.2/apps/backend/src/app.ts
+++ 0 to 1/Week 16/Week 16.2/apps/backend/src/app.ts
@@ -1,1 +1,11 @@
-const app = require('express')
+import express from "express"
+
+const app = express()
+
+app.get("/", (req, res)=>{
+    res.json({
+        messsage: "hello world!"
+    })
+})
+
+app.listen(3000)
