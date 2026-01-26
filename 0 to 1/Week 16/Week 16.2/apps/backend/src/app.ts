c426a7e9a6ad75cdb156a28408bf2e1f6fd97daf
Rishabh <rishabhbhatt956@gmail.com>
2026-01-26 09:02:57 +0530

    done

--- 0 to 1/Week 16/Week 16.2/apps/backend/src/app.ts
+++ 0 to 1/Week 16/Week 16.2/apps/backend/src/app.ts
@@ -1,5 +1,7 @@
 import express from "express"
 
+import { BACKEND_URL} from "@repo/common/config"
+console.log(BACKEND_URL)
 const app = express()
 
 app.get("/", (req, res)=>{
---
+++ 0 to 1/Week 16/Week 16.2/packages/common/src/index.ts
@@ -0,0 +1,4 @@
+
+export const VALUE = "rishabh bhatt";
+
+export const BACKEND_URL = "https://api.google.com"
