42250d1491d2bf2318fcaa8fcc797eba84753359
Rishabh <rishabhbhatt956@gmail.com>
2026-02-22 09:31:25 +0530

    websockets with react

---
+++ 0 to 1/Week 19/Week 19.1/backend/index.js
@@ -0,0 +1,24 @@
+import WebSocket, { WebSocketServer } from 'ws';
+import http from 'http';
+const server = http.createServer(function (request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.end("hi there");
+});
+let usersCount = 0;
+const wss = new WebSocketServer({ server });
+wss.on('connection', function connection(socket) {
+    socket.on('error', console.error);
+    socket.on('message', function message(data, isBinary) {
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(data, { binary: isBinary });
+            }
+        });
+    });
+    console.log("Users connected!: ", ++usersCount);
+    socket.send('Hello! Message From Server!!');
+});
+server.listen(8080, function () {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+//# sourceMappingURL=index.js.map
